fe3843af4805fc2978b38ec272dd2ac1dbbd494c
Maia Materman <maiam6242@gmail.com>
2019-04-03 13:51:43 -0400

    created a mapKit controller class and added mapkit framework

---
+++ Joe Knows/Joe Knows/MapKitController.swift
@@ -0,0 +1,16 @@
+//
+//  MapKitController.swift
+//  Joe Knows
+//
+//  Created by Maia Materman on 4/3/19.
+//  Copyright © 2019 Annie Tor. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapKitController: UIViewController
+{
+    
+    
+}
