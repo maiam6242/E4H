0fd7a301ae2ef5265769d79a0e051477cc2652f0
Maia Materman <maiam6242@gmail.com>
2019-04-05 18:36:20 -0400

    IT WORKSSSSSSSSSS

--- Joe Knows/Joe Knows/CurrentLocationViewController.swift
+++ Joe Knows/Joe Knows/CurrentLocationViewController.swift
@@ -45,8 +45,9 @@ class CurrentLocationViewController: UIViewController, MKMapViewDelegate, CLLoca
         mapView.isScrollEnabled = true
         
         mapView.center = view.center
-        
+       
         view.addSubview(mapView)
+        view.sendSubviewToBack(mapView)
     }
     
     func determineCurrentLocation(){
@@ -87,14 +88,15 @@ class CurrentLocationViewController: UIViewController, MKMapViewDelegate, CLLoca
 
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-   
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+
 
 func MainToLoading(_ sender: Any) {
+    
         print("button pressed")
         //self.performSegue(withIdentifier: "MainToLoading", sender: self)
     }
